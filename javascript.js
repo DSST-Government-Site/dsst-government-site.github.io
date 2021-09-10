361a99c6673614560471f838f94abc5520cd701b
DSST-Government-Site <90342765+DSST-Government-Site@users.noreply.github.com>
2021-09-10 13:58:15 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -5,21 +5,21 @@ function onWindowResise() {
 	
 	document.getElementById("target").innerHTML = String(Math.min(window.innerWidth, window.innerHeight));
 	lowpri++;
-	report("", 1);
+	report("", "low");
 	report("actually both");
 }
 window.onresize = onWindowResise;
 
 var lowpri;
 function report(mess, lowpri) {
-	if (lowpri == null) {
-		document.getElementById("consolebutjank").innerHTML = document.getElementById("consolebutjank").innerHTML+mess+"\n";
-	} else {document.getElementById("conupdbutjank").innerHTML = "Low Priority Pings: " + lowpri;}
+	if (lowpri == "low") {
+		document.getElementById("conupdbutjank").innerHTML = "Low Priority Pings: " + lowpri;
+	} else {document.getElementById("consolebutjank").innerHTML = document.getElementById("consolebutjank").innerHTML+mess+"\n";}
 }
 
 
 window.onload = (event) => {
-	lowpri = 0;
+	//lowpri = 0;
 	onWindowResise();
 	report('page is fully loaded');
 }
