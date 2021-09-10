ce54ee045298e85319535fb2426a3b620439559e
DSST-Government-Site <90342765+DSST-Government-Site@users.noreply.github.com>
2021-09-10 11:02:22 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,18 @@
 function updateWinWid() {
 	document.getElementById("target").innerHTML = "just kidding it actually updates automatically";
 }
-
 function onWindowResise() {
 	document.getElementById("target").innerHTML = String(window.innerWidth);
 	report("Updated!")
 }
 window.onresize = onWindowResise;
+consbjnk = document.getElementById("consolebutjank").innerHTML
+function report(mess) {
+	consbjnk += mess+"\n";
+}
 
 
 window.onload = (event) => {
 	onWindowResise();
 	report('page is fully loaded');
-}
-
-consbjnk = document.getElementById("consolebutjank").innerHTML
-function report(mess) {
-	consbjnk += mess+"\n";
 }
