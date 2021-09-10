45f449b2ecef0bf3cc33dab6e2b4db58540b8ed3
DSST-Government-Site <90342765+DSST-Government-Site@users.noreply.github.com>
2021-09-10 10:49:38 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
 function updateWinWid() {
+	document.getElementById("target").innerHTML = "just kidding it actually updates automatically";
+}
+
+function onWindowResise() {
 	document.getElementById("target").innerHTML = String(window.innerWidth);
 }
+
+window.onresize = onWindowResise;
