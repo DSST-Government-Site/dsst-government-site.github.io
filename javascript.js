252dc37d588fe30ff11663cbafe7489927cea9e4
DSST-Government-Site <90342765+DSST-Government-Site@users.noreply.github.com>
2021-09-10 13:30:25 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,13 +2,14 @@ function updateWinWid() {
 	document.getElementById("target").innerHTML = "just kidding it actually updates automatically";
 }
 function onWindowResise() {
+	
 	document.getElementById("target").innerHTML = String(window.innerWidth);
 	report("Updated!")
 }
 window.onresize = onWindowResise;
 consbjnk = document.getElementById("consolebutjank").innerHTML
 function report(mess) {
-	consbjnk += mess+"<br>";
+	consbjnk += mess+"\n";
 }
 
 
