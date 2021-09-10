b8cedb772517e8b0d8686d6557bf64a4137ce5d4
DSST-Government-Site <90342765+DSST-Government-Site@users.noreply.github.com>
2021-09-10 11:20:01 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,18 +1,22 @@
-function updateWinWid() {
-	document.getElementById("target").innerHTML = "just kidding it actually updates automatically also test 4";
-}
-function onWindowResise() {
-	document.getElementById("target").innerHTML = String(window.innerWidth);
-	report("Updated!")
-}
-window.onresize = onWindowResise;
-//consbjnk = document.getElementById("consolebutjank").innerHTML
-function report(mess) {
-	document.getElementById("consolebutjank").innerHTML = document.getElementById("consolebutjank").innerHTML+mess+"\n";
-}
+<html>
 
+<head>
+	<title>DSST Byers</title>
+	<script src="./javascript.js"></script>
+	<style>
+	.center {
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+		width: 50%;
+	}
+	</style>
+</head>
+<body>
+	<img src="./image/branches.jpg" class="center">
+	<button onclick="updateWinWid()">Update the window width</button>
+	<p id="target">test part 3</p>
+	<p id="consolebutjank"></p>
+</body>
 
-window.onload = (event) => {
-	onWindowResise();
-	report('page is fully loaded');
-}
+</html>
