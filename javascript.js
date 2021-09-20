2551057c30dd8fda324785affcf33da862a665f5
DSST-Government-Site <90342765+DSST-Government-Site@users.noreply.github.com>
2021-09-20 11:39:28 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,16 +1,21 @@
+window.onload = (event) => {
+	document.getElementById("search-query").addEventListener("keyup", function(event) {
+		if (event.keyCode === 13) {
+			event.preventDefault();
+			document.getElementById("search-button").click();
+		}
+	});
+	mblock = document.getElementById("main-block");
+	onWindowResise();
+	report('page is fully loaded');
+}
+
 function onWindowResise() {
-	mblock = document.getElementById("main-block")
-	mblock.style["max-width"] = window.innerHeight*1.25;
-	document.getElementById("target").innerHTML = String(window.innerHeight);
+	mblock.style["max-width"] = window.innerHeight * 1.25;
+	//document.getElementById("main-text").style["min-height"] = window.innerHeight
 }
 window.onresize = onWindowResise;
 
 function activateSearch(imnp) {
 	alert(document.getElementById("search-query").value)
-}
-
-window.onload = (event) => {
-	//lowpri = 0;
-	onWindowResise();
-	report('page is fully loaded');
 }
